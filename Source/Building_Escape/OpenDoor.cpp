541d2dd24850d3ed8cc25f525890b354272216e7
Jason ShuJi Chen <Shuji.chen1997@gmail.com>
2021-09-18 23:36:47 -0600

    Update
    - Added open/close trigger volume logic
    - Inherited DefaultPawn and Game Mode BP

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -1,7 +1,9 @@
 // Copyright Jason ShuJi Chen 2021
 
-#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -20,7 +22,15 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw += InitialYaw;
+	OpenAngle += InitialYaw;
+
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressure plate set."), *GetOwner()->GetName());
+	}
+	
+	// Sets the player pawn as the actor.
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
@@ -29,16 +39,38 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
-	UE_LOG(LogTemp, Warning, TEXT("The Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{	
+		OpenDoor(DeltaTime);
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+	else
+	{
+		if (GetWorld()->GetTimeSeconds() - DoorLastOpened >= DoorCloseDelay)
+		{
+			CloseDoor(DeltaTime);
+		}
 	
-	// CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
-	// FInterpTo is smoother, framerate independent.
+	}
 
+}
+
+void UOpenDoor::OpenDoor(float DeltaTime)
+{
 	// Set actor rotation
-	CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2);
+	// FInterpTo is smoother, framerate independent.
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, OpenAngle, DeltaTime, OpenSpeed);
 	FRotator DoorRotation = GetOwner()->GetActorRotation();
 	DoorRotation.Yaw = CurrentYaw;
 	GetOwner()->SetActorRotation(DoorRotation);
 }
 
+void UOpenDoor::CloseDoor(float DeltaTime)
+{
+	// Set actor rotation
+	// FInterpTo is smoother, framerate independent.
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, InitialYaw, DeltaTime, CloseSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+}
--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -15,7 +16,7 @@ class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UOpenDoor();
-
+	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -23,6 +24,8 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
 
 private:
 
@@ -30,7 +33,22 @@ private:
 	float CurrentYaw;
 
 	UPROPERTY(EditAnywhere);
-	float TargetYaw = 90.f;
-	
+	float OpenSpeed = 2;
 
+	UPROPERTY(EditAnywhere);
+	float CloseSpeed = 2;
+
+	UPROPERTY(EditAnywhere);
+	float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere);
+	ATriggerVolume* PressurePlate;
+	
+	UPROPERTY(EditAnywhere);
+	AActor* ActorThatOpens;
+
+	float DoorLastOpened = 0.f;
+
+	UPROPERTY(EditAnywhere);
+	float DoorCloseDelay = 2.f;
 };
